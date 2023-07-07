2cd64bc61c1760d0e2f2e9cd4761968ead546caf
nazims-flow <nazimmazumder68@gmail.com>
2023-07-07 20:17:02 +0530

    setup.py and requirement

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    This function will return the list of requirements
+    '''
+
+    HYPEN_E_DOT="-e ."
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+        
+    return requirements
+
+setup(
+name='mlproject',
+version='0.0.1',
+author="Nazim",
+author_email='nazimmazumder68@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+
+)
